60bf9175ac51e83e8ca5d2b9e5855ab58121829c
Nobuhito Ibaraki <nobuhito.ibaraki@zozo.com>
2019-12-17 18:40:58 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
-function benchmark(n, block) {
-  const startTime = Date.now();
+async function benchmark(n, block, pre) {
+  let total = 0;
   for (i = 0; i < n; i++) {
-    block(i)
+    let value;
+    if (pre) {
+      value = pre()
+    }
+    const startTime = Date.now();
+    await block(i, value)
+    const endTime = Date.now();
+    total += endTime - startTime
   }
-  const endTime = Date.now();
 
   return {
-    total: (endTime - startTime) / n
+    total: total / n
   }
 }
 
-function generateSample(nDim) { // row * column
-  const sample = []
-  for (let i = 0; i < nDim; i++) {
-    sample[i] = []
-    for (let j = 0; j < nDim; j++) {
-      sample[i][j] = Math.random()
-    }
+function generateFlopsSamples(nSample) {
+  const s = new Float32Array(nSample)
+  for (let i = 0; i < nSample; i++) {
+    s[i] = Math.random()
   }
-  return sample
+  return s
+}
+
+function generateDotProductSamples(nDim) { // row * column
+  function generateSample() {
+    const sample = []
+    for (let i = 0; i < nDim; i++) {
+      sample[i] = []
+      for (let j = 0; j < nDim; j++) {
+        sample[i][j] = Math.random()
+      }
+    }
+    return sample
+  }
+  return [generateSample(), generateSample()]
 }
 
 function dotProductJS(a, b, nDim) { // C = A . B
@@ -35,43 +52,128 @@ function dotProductJS(a, b, nDim) { // C = A . B
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-async function start() {
+const FLOPS_N_SAMPLE = 100000
+const FLOPS_SAMPLES = generateFlopsSamples(FLOPS_N_SAMPLE)
+const FLOPS_COUNT = 10
 
-  const nDim = 100;
+const DOT_PRODUCT_N_DIM = 1000;
+const [DOT_PRODUCT_SAMPLE_A, DOT_PRODUCT_SAMPLE_B] = generateDotProductSamples(DOT_PRODUCT_N_DIM)
+const DOT_PRODUCT_COUNT = 10
 
-  const sampleA = generateSample(nDim)
-  const sampleB = generateSample(nDim)
-  // const sampleA = new Float32Array([1, 2, 3, 4])
-  // const sampleB = new Float32Array([5, 6, 7, 8])
-  // const answer = new Float32Array([19, 22, 43, 50])
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  const nSample = 2
+async function benchmarkFLOPSJavaScript() {
+  const { total } = await benchmark(FLOPS_COUNT, () => {
+    let result = 0;
+    for (let i = 0; i < FLOPS_N_SAMPLE; i++) {
+      result += FLOPS_SAMPLES[i]
+    }
+  })
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  console.log('flops js: ', total)
+}
 
-  const abSampleA = new Float32Array(sampleA.flat())
-  const abSampleB = new Float32Array(sampleB.flat())
+async function benchmarkFLOPSGPU() {
+  const { total } = await benchmark(FLOPS_COUNT, async (_, value) => {
+    await value.data()
+  }, () => {
+    let result = tf.scalar(0);
+    for (let i = 0; i < FLOPS_N_SAMPLE; i++) {
+      result.add(FLOPS_SAMPLES[i])
+    }
+    return result
+  })
 
-  // console.log(abSampleA, abSampleB)
+  console.log('flops gpu: ', total)
+}
 
-  const { total: jsTotal } = benchmark(nSample, () => {
-    dotProductJS(abSampleA, abSampleB, nDim)
+async function benchmarkDotProductJavaScript() {
+  const abSampleA = new Float32Array(DOT_PRODUCT_SAMPLE_A.flat())
+  const abSampleB = new Float32Array(DOT_PRODUCT_SAMPLE_B.flat())
+
+  const { total: jsTotal } = await benchmark(DOT_PRODUCT_COUNT, () => {
+    dotProductJS(abSampleA, abSampleB, DOT_PRODUCT_N_DIM)
   })
 
   console.log('js: ', jsTotal)
+}
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////////
+async function benchmarkDotProductGPU() {
+  const tfSampleA = tf.tensor2d(DOT_PRODUCT_SAMPLE_A);
+  const tfSampleB = tf.tensor2d(DOT_PRODUCT_SAMPLE_B);
 
-  const tfSampleA = tf.tensor2d(sampleA);
-  const tfSampleB = tf.tensor2d(sampleB);
-
-  const { total: tfTotal } = benchmark(nSample, () => {
-    c = tf.dot(tfSampleA, tfSampleB)
-    console.log({...c})
+  const { total: tfTotal } = await benchmark(DOT_PRODUCT_COUNT, async (_, value) => {
+    await value.data()
+  }, () => {
+    return tf.dot(tfSampleA, tfSampleB)
   })
 
   console.log('tf: ', tfTotal)
-
 }
 
-start()
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const loadingDOM = document.getElementById('loading')
+const buttonStartFLOPSJavaScriptDOM = document.getElementById('button-start-flops-js')
+const buttonStartFLOPSGPUDOM = document.getElementById('button-start-flops-gpu')
+const buttonStartFLOPSWebAssemblyDOM = document.getElementById('button-start-flops-wasm')
+const buttonStartDotProductJavaScriptDOM = document.getElementById('button-start-dot-product-js')
+const buttonStartDotProductGPUDOM = document.getElementById('button-start-dot-product-gpu')
+const buttonStartDotProductWebAssemblyDOM = document.getElementById('button-start-dot-product-wasm')
+
+const startBenchmarkIfNeeded = (() => {
+  let isBenchmarking = false
+
+  return (benchmark) => {
+    if (isBenchmarking) return
+    isBenchmarking = true
+
+    loadingDOM.style.visibility = 'visible'
+    buttonStartFLOPSJavaScriptDOM.disabled = true
+    buttonStartFLOPSGPUDOM.disabled = true
+    buttonStartFLOPSWebAssemblyDOM.disabled = true
+    buttonStartDotProductJavaScriptDOM.disabled = true
+    buttonStartDotProductGPUDOM.disabled = true
+    buttonStartDotProductWebAssemblyDOM.disabled = true
+
+    setTimeout(async () => {
+      await benchmark()
+
+      setTimeout(() => {
+        isBenchmarking = false
+        loadingDOM.style.visibility = 'hidden'
+      }, 500)
+
+      buttonStartFLOPSJavaScriptDOM.disabled = false
+      buttonStartFLOPSGPUDOM.disabled = false
+      buttonStartFLOPSWebAssemblyDOM.disabled = false
+      buttonStartDotProductJavaScriptDOM.disabled = false
+      buttonStartDotProductGPUDOM.disabled = false
+      buttonStartDotProductWebAssemblyDOM.disabled = false
+    }, 500)
+  }
+})()
+
+buttonStartFLOPSJavaScriptDOM.addEventListener('click', () => {
+  startBenchmarkIfNeeded(() => {
+    benchmarkFLOPSJavaScript()
+  })
+})
+
+buttonStartFLOPSGPUDOM.addEventListener('click', () => {
+  startBenchmarkIfNeeded(() => {
+    benchmarkFLOPSGPU()
+  })
+})
+
+buttonStartDotProductJavaScriptDOM.addEventListener('click', () => {
+  startBenchmarkIfNeeded(() => {
+    benchmarkDotProductJavaScript()
+  })
+})
+
+buttonStartDotProductGPUDOM.addEventListener('click', () => {
+  startBenchmarkIfNeeded(() => {
+    benchmarkDotProductGPU()
+  })
+})
